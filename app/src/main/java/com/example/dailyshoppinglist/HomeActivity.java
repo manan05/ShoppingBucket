0da06d6ae676d27813992b03bf9706d1230da60f
Shivam Tyagi <shivamtyagi837@gmail.com>
2019-09-08 01:45:10 +0530

    .

--- app/src/main/java/com/example/dailyshoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/dailyshoppinglist/HomeActivity.java
@@ -1,14 +1,22 @@
 package com.example.dailyshoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mToolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Daily Shopping List");
     }
+
 }
