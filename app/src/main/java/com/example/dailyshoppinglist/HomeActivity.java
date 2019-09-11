bbc857f776b81f1adf199deaaf8fd6f1eeafa9e3
Shivam Tyagi <shivamtyagi837@gmail.com>
2019-09-11 20:58:22 +0530

    floating button functionality added

--- app/src/main/java/com/example/dailyshoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/dailyshoppinglist/HomeActivity.java
@@ -3,12 +3,17 @@ package com.example.dailyshoppinglist;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.w3c.dom.Text;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -44,9 +49,39 @@ public class HomeActivity extends AppCompatActivity {
 
         View myView = inflater.inflate(R.layout.input_layout,null);
 
-        AlertDialog dialog = myDialog.create();
+        final AlertDialog dialog = myDialog.create();
 
         dialog.setView(myView);
+
+        final EditText mType = myView.findViewById(R.id.et_type);
+        final EditText mAmount = myView.findViewById(R.id.et_amount);
+        final EditText mNote =  myView.findViewById(R.id.et_note);
+
+        Button mSaveBtn = myView.findViewById(R.id.btn_save);
+
+        mSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String type = mType.getText().toString().trim();
+                String amount = mAmount.getText().toString().trim();
+                String note = mNote.getText().toString().trim();
+
+                if(TextUtils.isEmpty(type)){
+                    mType.setError("Required field ...");
+                    return;
+                }
+                if(TextUtils.isEmpty(amount)){
+                    mAmount.setError("Required field ...");
+                    return;
+                }
+                if(TextUtils.isEmpty(note)){
+                    mNote.setError("Required field ...");
+                    return;
+                }
+                dialog.dismiss();
+            }
+        });
 
         dialog.show();
     }
