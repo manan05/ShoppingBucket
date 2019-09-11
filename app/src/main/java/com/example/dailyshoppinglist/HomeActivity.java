8e5bfce643eacb38af90748e79e4e89706d9bdae
Shivam Tyagi <shivamtyagi837@gmail.com>
2019-09-11 20:45:32 +0530

    custom dialog floating button

--- app/src/main/java/com/example/dailyshoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/dailyshoppinglist/HomeActivity.java
@@ -1,13 +1,20 @@
 package com.example.dailyshoppinglist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+
+    public FloatingActionButton fab_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,30 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Daily Shopping List");
 
+        fab_btn =  findViewById(R.id.fab);
+
+        fab_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customDialog();
+            }
+        });
+
+    }
+
+    private  void customDialog(){
+
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(HomeActivity.this);
+
+        LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+
+        View myView = inflater.inflate(R.layout.input_layout,null);
+
+        AlertDialog dialog = myDialog.create();
+
+        dialog.setView(myView);
+
+        dialog.show();
     }
 
 }
